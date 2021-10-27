e966fb2b4fa1eaf54f1ce498b8f5ea08a9b271a0
Artem <Artem@LP-38>
2021-10-27 13:31:55 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello" << endl; // Вывод Hello в консоль
+	cout << "Hello2" << endl; // Вывод Hello2 в консоль
+
+	return 0;
+}
